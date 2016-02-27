d95d811158e80fcbd3773ca7f0db49ee71bd205e
Jake Piccolo <jakepic1@gmail.com>
2016-02-27 14:24:07 -0800

    Add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-snapshot "0.1.0-SNAPSHOT"
   :description "Take snapshots of clojure reference types to disk."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jakepic1/clj-snapshot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
